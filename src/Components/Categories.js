8d9d3124f0a0b64c9aa41ce68afd4542e1c2aea1
anas138 <83574472+anas138@users.noreply.github.com>
2021-06-27 13:19:55 +0500

    added
    added

--- src/Components/Categories.js
+++ src/Components/Categories.js
@@ -8,6 +8,12 @@ import {Link} from 'react-router-dom'
 
 
 function Categories() {
+    const [cat,setCat]=useState(0);
+    const [subCatcat,setSubCat]=useState(0);
+    const [changeInput,setChangeInput]=useState('');
+    const [changeSubCat,setChangeSubCat]=useState('');
+    const [catFk,setCatFk]=useState('');
+    const [subCatData,setSubCatData]=useState([]);
     const change=useSelector(state=>state.ChangeCat);
     const UseDispatch=useDispatch();
     useEffect(()=>{
@@ -17,14 +23,18 @@ function Categories() {
             UseDispatch(Change(res.data));
             //console.log(change)
         })
+        axios.get('http://localhost:3001/subCat')
+        .then(res=>{
+            console.log(res.data);
+            setSubCatData(res.data);
+            //console.log(change)
+        })
 
     },[])
 
 
 
-    const [cat,setCat]=useState(0);
     
-    const [changeInput,setChangeInput]=useState('');
     
     
     const click=()=>{
@@ -41,13 +51,24 @@ function Categories() {
         //console.log('anas');
         setChangeInput(e.target.value);
     }
+    const subClickHandle=()=>{
+        console.log(catFk);
+        axios.post('http://localhost:3001/subCat',{
+            name:changeSubCat,
+            fkCat:catFk
+                  
+        }).then(res=>{
+            console.log(res);
+        });
+    }
     return (
         
         <div className='container'>
             <div>
             <div id='catDiv1'>
                <h1>Categories</h1>
-               <FaRegPlusSquare id='addCategory' onClick={()=>{setCat(1)}}/>
+               <FaRegPlusSquare id='addCategory' onClick={()=>{setCat(1);
+            setSubCat(0)}}/>
                </div>
                {cat==1?<form id='category-form'>
                    <buton type='button' className="btn btn-danger" onClick={()=>{setCat(0)}}>X</buton>
@@ -58,6 +79,18 @@ function Categories() {
                    <button type="button" className="btn btn-primary" id='form-button' onClick={click}>Submit</button>
                </form>
                : ''}
+               {subCatcat==1?
+                <form id='category-form'>
+                <buton type='button' className="btn btn-danger" onClick={()=>{setSubCat(0)}} >X</buton>
+                    <h2>Add sub category</h2>
+                    <input className="form-control" placeholder='Enter sub category' id='form-input' onChange={(e)=>{
+                        setChangeSubCat(e.target.value);
+                    }}></input>
+                    <input className="form-control"  id='form-input' type='file'></input>
+                    <button type="button" className="btn btn-primary" id='form-button' onClick={subClickHandle} >Submit</button>
+ 
+                </form>:''}
+              
                
 
 
@@ -65,25 +98,39 @@ function Categories() {
             </div>
             <div  id='mainid'>
                 {change.map(index=>{
-                   return <div>
+                   return <div id='subcat2'>
                      <div id='catDiv'>
                            <h2 id='categories'>{index.name}</h2>
                            </div>
-                            <div id='main'>
+                            
                             <div id='items'>
-                                <Link to='/categories/items'>
-                                <img src='download (4).jpg' id='catImage'/>
-                                </Link>
-                                <h5 id='heading'>{index.name}</h5>
+                            <FaRegPlusSquare id='addCategory' onClick={()=>{
+                                setSubCat(1);
+                                setCat(0);
+                                setCatFk(index._id);
+                            }} /> 
+                            {subCatData.map(index2=>{
+                                
+                                if(index2.fkCat==index._id){
+                                    return <div id='subcat2' >                             
+                                    <Link to='/categories/items'>
+                                    <img src='download (4).jpg' id='catImage' onClick={()=>{localStorage.setItem('subCatId',index2._id)}}/>
+                                    </Link>
+                                    <h5 id='heading'>{index2.name}</h5>
+                                    </div> 
+                                }
+                            })} 
+                            
+
                             </div>
             
                            
                             </div>
-                            </div>
+                            
 
                 })}
                 
-               
+                
             </div>
                       
             
--- src/Components/CategoryItems.js
+++ src/Components/CategoryItems.js
@@ -1,8 +1,42 @@
-import React,{useState} from 'react'
+import axios from 'axios';
+import React,{useState,useEffect} from 'react'
 import { FaRegPlusSquare } from "react-icons/fa";
+
 
 function CategoryItems() {
     const [cat,setCat]=useState(0);
+    const [changeName,setChangeName]=useState('');
+    const [changeWeight,setChangeWeight]=useState('');
+    const [changePrice,setChangePrice]=useState('');
+    const[itemData,setItemData]=useState([]);
+    const[addCart,setAddCart]=useState([]);
+    
+     useEffect(()=>{
+         axios.get('http://localhost:3001/items').then(res=>{
+            setItemData(res.data);
+         })
+     })
+    const handleClick=()=>{
+        axios.post('http://localhost:3001/items',{
+            name:changeName,
+            weight:changeWeight,
+            price:changePrice,
+            fkSubCat:localStorage.getItem('subCatId')
+        }).then(res=>{
+            console.log(res.data);
+        })
+    }
+    const handleAddToCart=(e)=>{
+        
+        let data=(localStorage.getItem('cartItems'));
+        
+          let res = data.concat(',',e.target.value)
+        console.log(res);
+        
+        
+     localStorage.setItem('cartItems',res.toString());
+
+    }
     return (
         <div className='container'>
 
@@ -10,17 +44,38 @@ function CategoryItems() {
             
       {cat==1? <form id='category-form'>
                 <buton type='button' className="btn btn-danger" onClick={()=>{setCat(0)}}>X</buton>
-                    <input className='form-control'  placeholder='Enter name'></input>
-                    <input className='form-control'  placeholder='Enter weight'></input>
-                    <input className='form-control'  placeholder='Enter price'></input>
-                    <button type="button" className="btn btn-primary" id='form-button' >Submit</button>
+                    <input className='form-control'  placeholder='Enter name' onChange={(e)=>{setChangeName(e.target.value)}}></input>
+                    <input className='form-control'  placeholder='Enter weight' onChange={(e)=>{setChangeWeight(e.target.value)}}></input>
+                    <input className='form-control'  placeholder='Enter price' onChange={(e)=>{setChangePrice(e.target.value)}}></input>
+                    <button type="button" className="btn btn-primary" id='form-button' onClick={handleClick} >Submit</button>
                 </form>
        :''}
                     
             
             <div>
-                <FaRegPlusSquare id='addCategory' onClick={()=>{setCat(1)}}/>
+                <FaRegPlusSquare id='addCategory' onClick={()=>{setCat(1)}}/>    
             </div>
+            
+                 <div className='row itemDiv'>
+                    {itemData.map(index=>{
+                if(index.fkSubCat==localStorage.getItem('subCatId'))
+                {
+                       return <div className=' col-2 itemDiv1'>
+                        <div className='imgdiv'>
+                        <img src='download (4).jpg'/>
+                        </div>
+                        <label className='nameItems'>{index.name}</label>
+                        <label className='nameItems'>{index.weight}gm</label>
+                        <label className='nameItems'>Rs. {index.price}</label>
+                        <button type="button" className="btn btn-primary" id='form-button' value={index._id} onClick={handleAddToCart} >ADD TO CART</button>
+                        </div>
+                }   
+            })} 
+                    </div>
+                    
+                    
+            
+            
         </div>
     )
 }
